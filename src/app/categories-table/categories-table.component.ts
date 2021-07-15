88752c137f07ec68915d8f453b69a44d1b880667
Ahmeeed22 <78164396+Ahmeeed22@users.noreply.github.com>
2021-07-15 16:45:48 +0200

    category

--- src/app/categories-table/categories-table.component.ts
+++ src/app/categories-table/categories-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'app/service/category.service';
+import { Observable,BehaviorSubject } from 'rxjs';
+
 
 @Component({
   selector: 'app-categories-table',
@@ -6,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories-table.component.css']
 })
 export class CategoriesTableComponent implements OnInit {
+  category:any;
+  formData: FormData = new FormData();
+  constructor(private _CategoryService:CategoryService) {
 
-  constructor() { }
+
+
+   }
+
+  
 
   ngOnInit(): void {
+    this.getAllCat();
   }
 
+
+
+  async getAllCat(){
+    let x:any;
+      await this._CategoryService.getAllCategory().subscribe(res=>{
+       x = res;
+       this.category = [];
+        console.log(x);
+        for (const cat of res) {
+            
+              this.category.push(cat);
+            
+        }
+      },err=>{console.log(err)}) 
+
 }
+
+async addCat(title:any,decs:any){
+ let dataa={
+    title:title,
+    decs:decs
+  }
+  
+ let data= await this._CategoryService.addNewCategory(dataa).subscribe((res)=>{
+
+   console.log(res);
+   this.getAllCat()
+  },(err)=>{console.log(err)})
+
+}
+
+
+
+}
+   
