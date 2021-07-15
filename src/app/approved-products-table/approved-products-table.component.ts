939e70351a5b4f5965143fefa55cbddaa0337833
Abd El Rahman Osama <aerosama793@gmail.com>
2021-07-15 16:53:08 +0200

    pending and accepted tables

--- src/app/approved-products-table/approved-products-table.component.ts
+++ src/app/approved-products-table/approved-products-table.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "app/service/product.service";
 
 @Component({
-  selector: 'app-approved-products-table',
-  templateUrl: './approved-products-table.component.html',
-  styleUrls: ['./approved-products-table.component.css']
+  selector: "app-approved-products-table",
+  templateUrl: "./approved-products-table.component.html",
+  styleUrls: ["./approved-products-table.component.css"],
 })
 export class ApprovedProductsTableComponent implements OnInit {
+  productsList: any = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.getAcceptedProducts();
   }
 
+  async getAcceptedProducts() {
+    await this.productService.AllProduct().subscribe((productResponse: any) => {
+      for (let product of productResponse) {
+        if (product.status_adminstaration === "accepted") {
+          this.productsList.push(product);
+        }
+      }
+      console.log(this.productsList);
+    });
+  }
+
+  async onDeleteProduct(id: any) {
+    await this.productService.DeleteProduct(id).subscribe((response: any) => {
+      this.getAcceptedProducts();
+    });
+  }
 }
