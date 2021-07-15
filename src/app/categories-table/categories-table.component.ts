ed37754ad201d8884d7f1e19498ac12bfac7e9ca
Abd El Rahman Osama <aerosama793@gmail.com>
2021-07-15 17:27:09 +0200

    user table and dashboard

--- src/app/categories-table/categories-table.component.ts
+++ src/app/categories-table/categories-table.component.ts
@@ -1,60 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { CategoryService } from 'app/service/category.service';
-import { Observable,BehaviorSubject } from 'rxjs';
-
+import { Component, OnInit } from "@angular/core";
+import { CategoryService } from "app/service/category.service";
+import { Observable, BehaviorSubject } from "rxjs";
 
 @Component({
-  selector: 'app-categories-table',
-  templateUrl: './categories-table.component.html',
-  styleUrls: ['./categories-table.component.css']
+  selector: "app-categories-table",
+  templateUrl: "./categories-table.component.html",
+  styleUrls: ["./categories-table.component.css"],
 })
 export class CategoriesTableComponent implements OnInit {
-  category:any;
+  category: any;
   formData: FormData = new FormData();
-  constructor(private _CategoryService:CategoryService) {
-
-
-
-   }
-
-  
+  constructor(private _CategoryService: CategoryService) {}
 
   ngOnInit(): void {
     this.getAllCat();
   }
 
-
-
-  async getAllCat(){
-    let x:any;
-      await this._CategoryService.getAllCategory().subscribe(res=>{
-       x = res;
-       this.category = [];
+  async getAllCat() {
+    let x: any;
+    await this._CategoryService.getAllCategory().subscribe(
+      (res) => {
+        x = res;
+        this.category = [];
         console.log(x);
         for (const cat of res) {
-            
-              this.category.push(cat);
-            
+          this.category.push(cat);
         }
-      },err=>{console.log(err)}) 
-
-}
-
-async addCat(title:any,decs:any){
- let dataa={
-    title:title,
-    decs:decs
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
-  
- let data= await this._CategoryService.addNewCategory(dataa).subscribe((res)=>{
 
-   console.log(res);
-   this.getAllCat()
-  },(err)=>{console.log(err)})
+  async addCat(title: any, decs: any) {
+    let dataa = {
+      title: title,
+      decs: decs,
+    };
 
+    let data = await this._CategoryService.addNewCategory(dataa).subscribe(
+      (res) => {
+        console.log(res);
+        this.getAllCat();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
-
-
-
-}
-   
--- src/app/users-table/users-table.component.ts
+++ src/app/users-table/users-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'app/service/auth.service';
 
 @Component({
   selector: 'app-users-table',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-table.component.css']
 })
 export class UsersTableComponent implements OnInit {
-
-  constructor() { }
+user:any;
+  constructor(private myuserservice:AuthService,private myroute:Router) { }
 
   ngOnInit(): void {
+    this.getAllUser()
   }
+  async getAllUser(){
+     this.user=[];
+    await this.myuserservice.getAllUser().subscribe((res:any)=>{
+      for (const item of res) {
+        if(item.Role_name=="user"){
+          this.user.push(item);
+        }
+      }
 
+    },(err)=>{console.log(err)})
+  }
+  async deleteuser(id:any){
+    await this.myuserservice.deleteUser(id).subscribe(res=>{console.log(res)},err=>{console.log(err)})
+    this.myroute.navigateByUrl('/dashboard');
+  }
 }
